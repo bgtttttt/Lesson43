40e5cc3d43be6c9dddd01b1b69a7b39d9de6eeeb
bgtttttt <bgtttttt007gmail.com>
2024-02-21 17:44:46 +0300

    test1

---
+++ Task05.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int* create(int size) {
+	int* array = new int[size];
+	return array;
+}
+void destroy(int* array) {
+	delete[] array;
+}
+int* test1() {
+	int* a = new int;
+	*a = 100;
+	return a;
+}
+int* test2() {
+	int a = 100;
+	return &a;
+}
+void test(double n) {
+	n * 10;
+}
+
+int main() {
+	int* pa = test1();
+	cout << *pa << "\n" << test1 << "\n";
+
+	sin(10);
+	cos(100);
+	test(10.5);
+	test(20.4);
+	test(30.3);
+
+	cout << *pa;
+
+	return 0;
+}
