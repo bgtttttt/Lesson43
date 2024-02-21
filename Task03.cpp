34ac54e015d17f612cd80dc6363eba714a0dfddf
bgtttttt <bgtttttt007gmail.com>
2024-02-21 17:28:23 +0300

    swap function

--- Task03.cpp
+++ Task03.cpp
@@ -1,17 +1,22 @@
-#include <iostream>
-
-using namespace std;
-
-void change(int* number) {
-	*number *= 2;
-}
-
-int main() {
-	int num = 15;
-
-	cout << "Before: " << num << "\n";
-	change(&num);
-	cout << "After: " << num << "\n";
-
-	return 0;
-}
+//#include <iostream>
+//
+//using namespace std;
+//
+//void change(int* number) {
+//	*number *= 2;
+//}
+//
+//int change(int number) {
+//	number *= 2;
+//	return number;
+//}
+//
+//int main() {
+//	int num = 15;
+//
+//	cout << "Before: " << num << "\n";
+//	change(&num);
+//	cout << "After: " << num << "\n";
+//
+//	return 0;
+//}
---
+++ Task04.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+void swap(int* number1, int* number2) {
+	int t = *number1;
+	*number1 = *number2;
+	*number2 = t;
+}
+
+int main() {
+	int x = 10, y = 20;
+
+	cout << "Before:\nx=" << x << "\ny = " << y << "\n";
+	swap(&x, &y);
+	cout << "\nAfter:\nx=" << x << "\ny = " << y << "\n";
+
+	return 0;
+}
